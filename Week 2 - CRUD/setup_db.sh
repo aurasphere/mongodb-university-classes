3d6e7f600d61764938e64c1c97ffffcac3f4493d
Donato Rimenti <donatohan.rimenti@gmail.com>
2017-02-17 01:37:29 +0100

    Added batch and bat db scripts

---
+++ Week 2 - CRUD/setup_db.sh
@@ -0,0 +1,2 @@
+mongoimport --drop -d students -c grades week_2_crud.5ed3dbd9602d/grades.json 
+mongorestore week_2_crud.5ed3dbd9602d/creating_documents_56959fced8ca393add3abe66/creating_documents/dump
